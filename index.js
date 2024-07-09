8fbe672356df3b0c74fa13d7bc8583b46a4c2d0c
MissMurigi <vickymurigi001@gmail.com>
2024-07-09 16:22:08 +0300

    FL

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    const headquartersBlock = 42;
+    //////The Math. abs() function is a built-in JavaScript function that returns the absolute value of a number. The absolute value of a number is its distance from zero on a number line, so it always returns a positive number. javascript.
+    return Math.abs(pickupBlock - headquartersBlock);
+  }
+  
+  function distanceFromHqInFeet(pickupBlock) {
+    const blocks = distanceFromHqInBlocks(pickupBlock);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
